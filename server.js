660a166db2363fe65c1c1baa045b01d424bfe98f
Uzo Olisemeka <uzo@designbymobius.ca>
2018-12-03 16:04:41 -0500

    fix: instantiate express properly

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express'),
     ussd_codes = require( './ussd.json' ),
     port = process.env.PORT || 5000,
+    app = express(),
     http_server;
 
-express.get( '/', function( req, res ){
+app.get( '/', function( req, res ){
   res.send( 'hello world' );
 });
 
-http_server = express.listen( port, function(){
+http_server = app.listen( port, function(){
   console.log( 'action=port-listen port=' + port );
 });
